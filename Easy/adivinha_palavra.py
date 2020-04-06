8a12982d02943ff96d39ce1da9a149397e108e05
André Alves <andrealvesdc@gmail.com>
2020-04-06 07:41:30 -0300

    Jogo adivinha palavra
    Jogo adivinha palavra

---
+++ Easy/adivinha_palavra.py
@@ -0,0 +1,41 @@
+secreto = 'perfume'
+digitadas = []
+chances = 3
+
+while True:
+    if chances <= 0:
+        print('Você perdeu!!!')
+        break
+
+    letra = input('Digite uma letra: ')
+
+    if len(letra) > 1:
+        print('Ahh isso não vale digite apenas uma letra.')
+        continue
+
+    digitadas.append(letra)
+
+    if letra in secreto:
+        print(f'UHUUUULLL, a letra  "{letra}" existe na palavra secreta.')
+    else:
+        print(f'AFFFzzz: a letra "{letra}" NÃO EZISTE na palavra secreta.')
+        digitadas.pop()
+
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra_secreta
+        else:
+            secreto_temporario += '*'
+
+    if secreto_temporario == secreto:
+        print(f'Que legal, VOCÊ GANHOU!!!! A palavra secreta era {secreto_temporario}.')
+        break
+    else:
+        print(f'A palavra secreta esta assim: {secreto_temporario}')
+
+    if letra not in secreto:
+        chances -= 1
+
+    print(f'Você ainda tem {chances} chaces.')
+    print()
